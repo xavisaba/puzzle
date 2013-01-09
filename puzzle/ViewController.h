7b5dd479e2a49839990770918f458a171e17de9c
Xavier Sánchez <xavisaba@gmail.com>
2013-01-09 16:57:34 +0100

    Primera subida
    Primera subida

---
+++ puzzle/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  puzzle
+//
+//  Created by Xavi on 27/12/12.
+//  Copyright (c) 2012 Xavi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)handlePan:(UIPanGestureRecognizer *) recognizer;
+@end
---
+++ puzzle/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  puzzle
+//
+//  Created by Xavi on 27/12/12.
+//  Copyright (c) 2012 Xavi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+- (IBAction)handlePan:(UIPanGestureRecognizer *) recognizer{
+    CGPoint translation = [recognizer translationInView:self.view];
+    recognizer.view.center = CGPointMake(recognizer.view.center.x + translation.x,recognizer.view.center.y + translation.y);
+    [recognizer setTranslation:CGPointMake(0,0) inView:self.view];
+}
+@end
